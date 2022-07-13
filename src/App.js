d4c676f9bd147241e020df6d447a2b306cd17ee6
bryanballidev <bryanballidev@gmail.com>
2022-07-13 18:25:58 +0300

    frontend_fix

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ function App() {
       <div className='title'>Scheduled NFT mint page</div>
       <div className='form'>
         <div className='row'>
-          <div>
+          <div className='item'>
             <label>Contract*</label>
             <input type="text" placeholder='input contract address'/>
           </div>
-          <div>
+          <div className='item'>
             <label>Price in Eth*</label>
             <input type="text" placeholder='input price'/>
           </div>
-          <div>
+          <div className='item'>
             <label>Max Gas(Gwei)*</label>
             <input type="text" placeholder='please gas'/>
           </div>
         </div>
 
-        <div>
-          <div>
-            <label>Contract*</label>
+        <div className='row'>
+          <div className='item'>
+            <label>Function Name*</label>
+            <input type="text" placeholder='input function name'/>
+          </div>
+          <div className='item'>
+            <label>Priority Fee(Gwei)*</label>
             <input type="text" placeholder='please input your contract address'/>
+          </div>
+          <div className='item'>
+            <label>Accounts</label>
+            <input type="submit" value="Select Accounts"/>
           </div>
         </div>
       </div>
