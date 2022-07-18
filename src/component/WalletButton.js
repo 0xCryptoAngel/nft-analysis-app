ef5c5a9902e03221f3e5cd8f55de74ccbe5e67e4
bryanballidev <bryanballidev@gmail.com>
2022-07-18 19:30:11 +0300

    bug_fix

--- src/component/WalletButton.js
+++ src/component/WalletButton.js
@@ -76,7 +76,7 @@ const WalletButton = () => {
   const Modal = ({ handleClose, show, children }) => {
     const showHideClassName = show ? "block" : "hidden";
     return (
-      <div className={`bg-black bg-opacity-70 w-full h-full flex justify-center items-center fixed top-0 left-0 modal ${showHideClassName}`} onClick={handleClose}>
+      <div className={`bg-black bg-opacity-70 w-full h-full flex justify-center items-center fixed top-0 left-0 z-20 modal ${showHideClassName}`} onClick={handleClose}>
         <section className=" bg-blue-900 z-50 rounded-xl pb-6 pt-4 px-4 relative w-96">
           <div className="font-bold text-white text-2xl text-center py-2">Connect Wallet</div>
           {children}
