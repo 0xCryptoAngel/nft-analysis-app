78f3461e55438392acbb4c833b6d43ddd5027b9b
bryanballidev <bryanballidev@gmail.com>
2022-07-25 12:08:44 -0700

    chart_initial_fix

--- src/page/Analysis.js
+++ src/page/Analysis.js
@@ -19,188 +19,13 @@ import {
   Legend,
   Scatter
 } from "recharts";
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 590,
-//     pv: 800,
-//     amt: 1400,
-//     cnt: 490
-//   },
-//   {
-//     name: "Page B",
-//     uv: 868,
-//     pv: 967,
-//     amt: 1506,
-//     cnt: 590
-//   },
-//   {
-//     name: "Page C",
-//     uv: 1397,
-//     pv: 1098,
-//     amt: 989,
-//     cnt: 350
-//   },
-//   {
-//     name: "Page D",
-//     uv: 1480,
-//     pv: 1200,
-//     amt: 1228,
-//     cnt: 480
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1520,
-//     pv: 1108,
-//     amt: 1100,
-//     cnt: 460
-//   },
-//   {
-//     name: "Page F",
-//     uv: 1400,
-//     pv: 680,
-//     amt: 1700,
-//     cnt: 380
-//   }
-// ];
 
-const data1 = [
-  {
-    name: "Page A",
-    uv: 4000,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    amt: 2100
-  }
-];
-
-// const data2 = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100
-//   }
-// ];
-
-// const data3 = [
-//   { x: 100, y: 200, z: 200 },
-//   { x: 120, y: 100, z: 260 },
-//   { x: 170, y: 300, z: 400 },
-//   { x: 140, y: 250, z: 280 },
-//   { x: 150, y: 400, z: 500 },
-//   { x: 110, y: 280, z: 200 },
-// ];
-// const data4 = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: 'Page D',
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: 'Page E',
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: 'Page F',
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: 'Page G',
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
 const Analysis = () => {
   const [collection, setCollection] = useState([])
+  const [strength, setStrength] = useState([])
+  const [listing, setListing] = useState([])
+  const [nftOwner, setNftOwner] = useState([])
+  const [salesData, setSalesData] = useState([])
   useEffect(()=> {
     fetchCollection()
   }, [])
@@ -209,42 +34,40 @@ const Analysis = () => {
     console.log("openSeaData.data", openSeaData.data)
     setCollection(openSeaData.data)
   }
+  const fetchStrength = async ()=> {
+    const strengthData = await axios.get("https://api.nftinit.io/api/get_price_distribution/?c=2")
+    console.log("openSeaData.data", strengthData.data.items)
+    setStrength(strengthData.data.items)
+  }
+  const fetchListing = async () => {
+    const listingData = await axios.get("https://api.nftinit.io/api/chart/?password=Gunah4423_&slug=boredapeyachtclub&type=listed_count")
+    console.log("openSeaData.data", listingData.data)
+    setListing(listingData.data)
+  }
+  const fetchOwner = async () => {
+    const nftData = await axios.get("https://api.nftinit.io/api/get_nfts_per_owner/?c=2")
+    console.log("openSeaData.data", nftData.data.items)
+    setNftOwner(nftData.data.items)
+  }
+  const fetchSales = async () => {
+    const sales = await axios.get("https://api.nftinit.io/api/sale_chart/?slug=boredapeyachtclub&tc=true&tn=true")
+    console.log("openSeaData.data", sales.data.items)
+    let filter = sales.data.items?.filter(item => item.event_price < 10000)
+    setSalesData(filter)
+  }
+  
   return (
     <div>
       <div>
         <Tabs>
           <TabList>
-            {/* <Tab>Floor Strength</Tab> */}
             <Tab>Floor Price</Tab>
-            {/* <Tab>Active Listings</Tab>
-            <Tab>Sales / Ranking</Tab>
-            <Tab>Sales</Tab> */}
+            <Tab onClick={fetchStrength}>Floor Strength</Tab>
+            <Tab onClick={fetchListing}>Active Listings</Tab>
+            <Tab onClick={fetchOwner}>Nfts Per Owner</Tab>
+            <Tab onClick={fetchSales}>Sales / Ranking</Tab>
           </TabList>
-          {/* <TabPanel>
-            <ComposedChart
-              width={500}
-              height={400}
-              data={data}
-              margin={{
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 20
-              }}
-            >
-              <CartesianGrid stroke="#f5f5f5" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-              <Bar dataKey="pv" barSize={20} fill="#413ea0" />
-              <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-              <Scatter dataKey="cnt" fill="red" />
-            </ComposedChart>
-          </TabPanel> */}
           <TabPanel>
-            <ResponsiveContainer width="100%" height="100%">
               <AreaChart
                 width={1000}
                 height={800}
@@ -262,13 +85,12 @@ const Analysis = () => {
                 <Tooltip />
                 <Area type="linear" dataKey="floor_price" stroke="#8884d8" fill="#8884d8" />
               </AreaChart>
-              </ResponsiveContainer>
           </TabPanel>
-          {/* <TabPanel>
+          <TabPanel>
             <BarChart
-              width={500}
-              height={300}
-              data={data2}
+              width={1000}
+              height={800}
+              data={strength}
               margin={{
                 top: 5,
                 right: 30,
@@ -277,18 +99,56 @@ const Analysis = () => {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="price_range" />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="count" fill="#8884d8" />
+            </BarChart>
+          </TabPanel>
+          <TabPanel>
+            <AreaChart
+              width={1000}
+              height={800}
+              data={listing}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" vertical={false}/>
+              <XAxis dataKey="timestamp" />
+              <YAxis />
+              <Tooltip />
+              <Area type="linear" dataKey="listed_count" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
+          </TabPanel>
+          <TabPanel>
+            <BarChart
+              width={1000}
+              height={800}
+              data={nftOwner}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="nft" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="percantage" fill="#8884d8" />
             </BarChart>
           </TabPanel>
           <TabPanel>
             <ScatterChart
-              width={400}
-              height={400}
+              width={1000}
+              height={800}
               margin={{
                 top: 20,
                 right: 20,
@@ -297,31 +157,12 @@ const Analysis = () => {
               }}
             >
               <CartesianGrid />
-              <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-              <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+              <XAxis dataKey="event_date" name="stature" unit="cm" />
+              <YAxis dataKey="event_price" name="weight" unit="kg" />
               <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter name="A school" data={data3} fill="#8884d8" />
+              <Scatter name="A school" data={salesData} fill="#8884d8" />
             </ScatterChart>
           </TabPanel>
-          <TabPanel>
-            <AreaChart
-              width={500}
-              height={400}
-              data={data4}
-              margin={{
-                top: 10,
-                right: 30,
-                left: 0,
-                bottom: 0,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-            </AreaChart>
-          </TabPanel> */}
         </Tabs>
       </div>
     </div>
