6101fdbc482c71d3e95f32cf1478baa4c44c6e58
bryanballidev <bryanballidev@gmail.com>
2022-09-14 15:47:38 +0300

    fix

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -1,4 +1,6 @@
 export  default function randomColor() {
-  const colorType = ['#5F9EA0',	'#4682B4', '#B0C4DE', '#ADD8E6', '#B0E0E6',	'#87CEFA', '#87CEEB', '#6495ED',  '#00BFFF', '#1E90FF',	'#4169E1',	'#0000FF',	'#0000CD',	'#00008B',	'#000080', '#191970'];
-  return  colorType[Math.floor(Math.random() * 16)]
+  // const colorType = ['#5F9EA0',	'#4682B4', '#B0C4DE', '#ADD8E6', '#B0E0E6',	'#87CEFA', '#87CEEB', '#6495ED',  '#00BFFF', '#1E90FF',	'#4169E1',	'#0000FF',	'#0000CD',	'#00008B',	'#000080', '#191970'];
+  const colorType = ['#5F9EA0'];
+  // return  colorType[Math.floor(Math.random() * 16)]
+  return  colorType[0]
 }
