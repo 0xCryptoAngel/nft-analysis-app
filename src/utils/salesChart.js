1a98c14678ab6b1f05256911dd5334a9b151ed4e
bryanballidev <bryanballidev@gmail.com>
2022-09-14 15:57:39 +0300

    random_value_fix

--- src/utils/salesChart.js
+++ src/utils/salesChart.js
@@ -3,7 +3,7 @@ export  default function salesData(value) {
     referenceDate.setMonth(referenceDate.getMonth() - 1);
     var sum = 0;
     value.data.items?.forEach(function(num) { sum += num.event_price });
-    let average = 2 * sum / value.data.items.length;
+    let average = 1.5 * sum / value.data.items.length;
     let filter = value.data.items?.filter(item => item.event_price < average && new Date(referenceDate) < new Date(item.event_date))
   return  filter.reverse()
 }
